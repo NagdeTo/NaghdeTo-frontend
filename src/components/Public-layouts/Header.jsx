cf708c7e0cb04896ff6bd5b4b44a55343d8b0db4
Haniye Movahediyan <haniyaa79@yahoo.com>
2024-04-01 22:05:15 +0430

    completed Header

--- src/components/Public-layouts/Header.jsx
+++ src/components/Public-layouts/Header.jsx
@@ -1,3 +1,45 @@
+import logo from "../../assets/images/logo.svg";
+import { Link } from "react-router-dom";
+import { Button, Flex, Typography } from "antd";
+
 export default function Header() {
-  return <div></div>;
+  return (
+    <Flex
+      style={{ direction: "rtl" }}
+      component="header"
+      justify="space-between"
+      align="center"
+      className="px-24 py-6"
+    >
+      <Flex align="center">
+        <img src={logo} className=" w-28" />
+        <Typography className="text-xl font-bold">نقدتو</Typography>
+      </Flex>
+
+      <Flex justify="space-between" gap="56px" className="text-sm font-medium">
+        <Link to="/" className="hover:text-primary-700">
+          خانه
+        </Link>
+        <Link to="/" className="hover:text-primary-700">
+          نقدتو چیه؟
+        </Link>
+        <Link to="/" className="hover:text-primary-700">
+          قیمتامون چنده؟
+        </Link>
+        <Link to="/" className="hover:text-primary-700">
+          ما کی هستیم؟
+        </Link>
+        <Link to="/" className="hover:text-primary-700">
+          با ما در ارتباط باش
+        </Link>
+      </Flex>
+
+      <Button
+        className="h-12 w-32 rounded-lg border-primary-1000
+       bg-primary-1000 font-medium text-white hover:bg-primary-900 focus-visible:outline-none"
+      >
+        ورود / ثبت نام
+      </Button>
+    </Flex>
+  );
 }
